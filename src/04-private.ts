c8d2010582e4cf5b6b99faab05c63b5468fdc3ac
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-20 15:17:13 +0200

    feat: private fields and methods

---
+++ src/04-private.ts
@@ -0,0 +1,43 @@
+export class MyDate {
+  private year: number;
+  private month: number;
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  public printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return value;
+  }
+
+  // bad method but only for learn
+  public add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+}
+
+const myDate = new MyDate(2024, 4, 9);
+console.log(myDate.printFormat());
+myDate.add(3, 'days');
+console.log(myDate.printFormat());
+//myDate.year = 1999; -> here error because it's private :)
