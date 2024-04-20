e8ed96b059023cd93474121bf4b3a9b78e25a97f
Junior Paradelo <juniorparadelo@gmail.com>
2024-04-20 15:01:28 +0200

    feat: class in ts

---
+++ src/01-class.ts
@@ -0,0 +1,14 @@
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const myDate = new MyDate(2024, 4, 20);
+console.log(myDate);
